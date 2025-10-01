4a722578cb4ba17e105e955fca502f5c47a334cf
DoyoSaee <ehdgydi@gmail.com>
2025-10-02 00:31:54 +0900

    add : ws package

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 3100 });
+
+wss.on("connection", (ws) => {
+  ws.send("connected");
+  ws.on("message", (messageFromClient) => {
+    const message = JSON.parse(messageFromClient);
+    console.log("message", message);
+  });
+});
