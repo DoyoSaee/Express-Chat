3e38b78f39fe005b5e3c6ba6ca9b32ef852c4e89
DoyoSaee <ehdgydi@gmail.com>
2025-10-03 23:48:02 +0900

    fix : ws

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const wss = new WebSocket.Server({ port: 3100 });
 
 wss.on("connection", (ws) => {
   ws.send("connected");
+
   ws.on("message", (messageFromClient) => {
     const message = JSON.parse(messageFromClient);
     console.log("message", message);
