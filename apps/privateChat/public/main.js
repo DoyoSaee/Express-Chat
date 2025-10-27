6c1971a9d066a31c1cd6b267efce6e574a69cf6c
DoyoSaee <ehdgydi@gmail.com>
2025-10-28 08:50:18 +0900

    chore : create userSession(client)

--- apps/privateChat/public/main.js
+++ apps/privateChat/public/main.js
@@ -17,3 +17,44 @@ const userTagLine = document.querySelector("#user-tagline");
 const title = document.querySelector("#active-user");
 const messages = document.querySelector(".messages");
 const msdDiv = document.querySelector(".msd-form");
+
+//login form handler
+const loginForm = document.querySelector(".user-login");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const username = document.getElementById("username");
+  createSession(username.value.toLowerCase());
+  username.value = "";
+});
+
+const createSession = async (username) => {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username }),
+  };
+
+  await fetch("/session", options)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      socketConnect(data.username, data.userID);
+      //로컬스토리지에 세션을 저장
+      localStorage.setItem("session-username", data.username);
+      localStorage.setItem("session-userID", data.userID);
+      loginContainer.classList.add("d-none");
+      chatBody.classList.remove("d-none");
+      userTitle.classList.remove("d-none");
+    })
+    .catch((err) => console.log(err));
+};
+
+const socketConnect = async (username, userID) => {
+  socket.auth = {
+    username,
+    userID,
+  };
+  await socket.connect();
+};
